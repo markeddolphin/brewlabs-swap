557452b9421918e95266ca244302fea2ef22d0b7
millicare <lacosby.milli@gmail.com>
2023-06-22 12:01:05 -0400

    delete leftover files

--- src/pages/swap.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import Swap from "views/swap";
-
-const SwapPage: NextPage = () => {
-  return <Swap />;
-};
-
-export default SwapPage;
--- src/pages/zapper.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import Directory from "views/directory";
-
-const Zapper: NextPage = () => {
-  return <Directory page={4} />;
-};
-
-export default Zapper;
